9bf578d4c6f2fed797ba934143e64e86ae274707
HaDH <gusdk123123@naver.com>
2023-04-18 15:59:20 +0900

    10998번 문제 A*B

---
+++ src/main/java/org/example/InputOutput/Beakjoon_10998.java
@@ -0,0 +1,14 @@
+package org.example.InputOutput;
+
+import java.util.Scanner;
+
+public class Beakjoon_10998 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        System.out.println(A * B);
+    }
+}
