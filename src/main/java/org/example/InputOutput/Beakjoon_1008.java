2b722ba128f20e7cd70d8ef8b989424eb65dd651
HaDH <gusdk123123@naver.com>
2023-04-18 15:59:40 +0900

    1008번 문제 A/B

---
+++ src/main/java/org/example/InputOutput/Beakjoon_1008.java
@@ -0,0 +1,14 @@
+package org.example.InputOutput;
+
+import java.util.Scanner;
+
+public class Beakjoon_1008 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        double A = scanner.nextDouble();
+        double B = scanner.nextDouble();
+
+        System.out.println(A/B);
+    }
+}
