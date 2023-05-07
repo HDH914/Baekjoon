d8ec8c06fe0ce90a69d8f11df5dbddbd7a7ebf75
HaDH <gusdk123123@naver.com>
2023-05-07 17:13:51 +0900

    1316번 문제 그룹 단어 체커

---
+++ src/main/java/org/example/Intensive/Baekjoon_1316.java
@@ -0,0 +1,14 @@
+package org.example.Intensive;
+
+import java.util.Scanner;
+
+public class Baekjoon_1316 {
+    public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+
+        int T = scanner.nextInt();
+
+
+        }
+
+}
